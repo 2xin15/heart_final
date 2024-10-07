108e059e2aef815e603d755275949244430ec991
2xin15 <s111291519xin@gapp.nthu.edu.tw>
2024-10-07 14:00:08 +0800

    initial

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,34 @@
+
+
+
+const scriptsInEvents = {
+
+	async ["01-3_Event10"](runtime, localVars)
+	{
+		window.open('https://fabulousscottie.sirv.com/Heart%202/Heart%202.spin', '_blank', 'width=800,height=600');
+	},
+
+	async ["01-3_Event12"](runtime, localVars)
+	{
+		window.open('https://fabulousscottie.sirv.com/Heart%201/Heart%201.spin', '_blank', 'width=800,height=600');
+	},
+
+	async ["02-3_Event3"](runtime, localVars)
+	{
+		window.open('https://fabulousscottie.sirv.com/test1/test1.spin', '_blank', 'width=800,height=600');
+	},
+
+	async ["02-5_Event13"](runtime, localVars)
+	{
+		window.open('https://fabulousscottie.sirv.com/test3/test3.spin', '_blank', 'width=800,height=600');
+	},
+
+	async ["02-7_Event16"](runtime, localVars)
+	{
+		window.open('https://fabulousscottie.sirv.com/test2/test2.spin', '_blank', 'width=800,height=600');
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
